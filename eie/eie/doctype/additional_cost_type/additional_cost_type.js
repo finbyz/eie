0ff682e1c60c3b13e93a167e533bb862e99efe4a
finbyz <info@finbyz.com>
2021-02-09 05:52:35 +0000

    Additional Cost Type Doctype CHanges

---
+++ eie/eie/doctype/additional_cost_type/additional_cost_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, FinByz Tech Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Additional Cost Type', {
+	// refresh: function(frm) {
+
+	// }
+});
