6b9f1449741ec0ce8ddf200fb15acd2a27b385a8
Ubuntu <ubuntu@ip-172-31-28-100.ap-south-1.compute.internal>
2024-05-27 10:57:03 +0000

    NB in purchase order filter in supplier contact link with supplier name

--- eie/public/js/doctype_js/purchase_order.js
+++ eie/public/js/doctype_js/purchase_order.js
@@ -9,6 +9,11 @@ frappe.ui.form.on("Purchase Order", {
             });
         }
     },
-    
+});
+cur_frm.set_query("contact_person", function () {
+    return {
+        query: "frappe.contacts.doctype.contact.contact.contact_query",
+        filters: { link_doctype: "Supplier", link_name: cur_frm.doc.supplier }
+    };
 });
 // cur_frm.add_fetch("cost_center", "company", "cost_center");
