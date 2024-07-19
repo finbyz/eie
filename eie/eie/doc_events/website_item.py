9bd185275154f20bbb7f31a682afce76124f8704
finbyz <info@finbyz.com>
2024-07-19 06:41:03 +0000

    PS changes for website item

--- eie/eie/doc_events/website_item.py
+++ eie/eie/doc_events/website_item.py
@@ -1,98 +1,99 @@
 import frappe
 from frappe.utils import cint, cstr, flt, random_string
 import json
-# from erpnext.setup.doctype.item_group.item_group import (
-# 	get_parent_item_groups,
-# 	invalidate_cache_for,
-# )
+from webshop.webshop.doctype.override_doctype.item_group import (
+    get_parent_item_groups,
+    invalidate_cache_for,
+)
 import itertools
 from frappe.website.doctype.website_slideshow.website_slideshow import get_slideshow
-# from erpnext.e_commerce.doctype.item_review.item_review import get_item_reviews
-# from erpnext.e_commerce.doctype.website_item.website_item import WebsiteItem, check_if_user_is_customer
+from webshop.webshop.doctype.item_review.item_review import get_item_reviews
 
-class CustomWebsiteItem(WebsiteItem):
-	def get_context(self, context):
-			context.show_search = True
-			context.search_link = "/search"
-			context.body_class = "product-page"
+# class CustomWebsiteItem(WebsiteItem):
+# 	def get_context(self, context):
+# 			context.show_search = True
+# 			context.search_link = "/search"
+# 			context.body_class = "product-page"
 
-			context.variants = frappe.db.get_all("Item", filters={"variant_of": self.item_code})
-			variant = frappe.form_dict.variant
-			if not variant:
-				if self.has_variants:
-					variant = context.variants[0]
-			if variant:
-				context.variant = frappe.get_doc("Item", variant)
-				for fieldname in ("website_image", "website_image_alt", "web_long_description", "description",
-										"website_specifications"):
-					if context.variant.get(fieldname):
-						value = context.variant.get(fieldname)
-						if isinstance(value, list):
-							value = [d.as_dict() for d in value]
+# 			context.variants = frappe.db.get_all("Item", filters={"variant_of": self.item_code})
+# 			variant = frappe.form_dict.variant
+# 			if not variant:
+# 				if self.has_variants:
+# 					variant = context.variants[0]
+# 			if variant:
+# 				context.variant = frappe.get_doc("Item", variant)
+# 				for fieldname in ("website_image", "website_image_alt", "web_long_description", "description",
+# 										"website_specifications"):
+# 					if context.variant.get(fieldname):
+# 						value = context.variant.get(fieldname)
+# 						if isinstance(value, list):
+# 							value = [d.as_dict() for d in value]
 
-						context[fieldname] = value
-			# if self.has_variants:
-			# 	context.variant = context.variants[0]
-			context.parents = get_parent_item_groups(self.item_group, from_item=True)  # breadcumbs
+# 						context[fieldname] = value
+# 			# if self.has_variants:
+# 			# 	context.variant = context.variants[0]
+# 			context.parents = get_parent_item_groups(self.item_group, from_item=True)  # breadcumbs
 
-			context.attributes = self.attributes = frappe.get_all(
-				"Item Variant Attribute",
-				fields=["attribute", "attribute_value"],
-				filters={"parent": self.item_code},
-			)
-			set_attribute_context(self, context)
-			set_disabled_attributes(self, context)
-			if self.slideshow:
-				context.update(get_slideshow(self))
+# 			context.attributes = self.attributes = frappe.get_all(
+# 				"Item Variant Attribute",
+# 				fields=["attribute", "attribute_value"],
+# 				filters={"parent": self.item_code},
+# 			)
+# 			frappe.throw("I am Here")
+# 			set_attribute_context(self, context)
+# 			set_disabled_attributes(self, context)
+# 			if self.slideshow:
+# 				context.update(get_slideshow(self))
 				
-			# self.set_disabled_attributes(context)
-			self.set_metatags(context)
-			self.set_shopping_cart_data(context)
+# 			# self.set_disabled_attributes(context)
+# 			self.set_metatags(context)
+# 			self.set_shopping_cart_data(context)
 
-			settings = context.shopping_cart.cart_settings
+# 			settings = context.shopping_cart.cart_settings
 
-			self.get_product_details_section(context)
+# 			self.get_product_details_section(context)
 
-			if settings.get("enable_reviews"):
-				reviews_data = get_item_reviews(self.name)
-				context.update(reviews_data)
-				context.reviews = context.reviews[:4]
+# 			if settings.get("enable_reviews"):
+# 				reviews_data = get_item_reviews(self.name)
+# 				context.update(reviews_data)
+# 				context.reviews = context.reviews[:4]
 
-			context.wished = False
-			if frappe.db.exists(
-				"Wishlist Item", {"item_code": self.item_code, "parent": frappe.session.user}
-			):
-				context.wished = True
+# 			context.wished = False
+# 			if frappe.db.exists(
+# 				"Wishlist Item", {"item_code": self.item_code, "parent": frappe.session.user}
+# 			):
+# 				context.wished = True
 
-			context.user_is_customer = check_if_user_is_customer()
+# 			context.user_is_customer = check_if_user_is_customer()
 
-			context.recommended_items = None
-			if settings and settings.enable_recommendations:
-				context.recommended_items = self.get_recommended_items(settings)
+# 			context.recommended_items = None
+# 			if settings and settings.enable_recommendations:
+# 				context.recommended_items = self.get_recommended_items(settings)
 
-			return context
+# 			return context
 
-	def check_if_user_is_customer(user=None):
-		from frappe.contacts.doctype.contact.contact import get_contact_name
+# 	def check_if_user_is_customer(user=None):
+# 		from frappe.contacts.doctype.contact.contact import get_contact_name
 
-		if not user:
-			user = frappe.session.user
+# 		if not user:
+# 			user = frappe.session.user
 
-		contact_name = get_contact_name(user)
-		customer = None
+# 		contact_name = get_contact_name(user)
+# 		customer = None
 
-		if contact_name:
-			contact = frappe.get_doc("Contact", contact_name)
-			for link in contact.links:
-				if link.link_doctype == "Customer":
-					customer = link.link_name
-					break
+# 		if contact_name:
+# 			contact = frappe.get_doc("Contact", contact_name)
+# 			for link in contact.links:
+# 				if link.link_doctype == "Customer":
+# 					customer = link.link_name
+# 					break
 
-		return True if customer else False
+# 		return True if customer else False
 
 def set_attribute_context(self, context):
 		if not self.has_variants:
 			return
+		
 
 		attribute_values_available = {}
 		context.attribute_values = {}
--- eie/eie/report/eie_gst_itemised_sales_register/eie_gst_itemised_sales_register.py
+++ eie/eie/report/eie_gst_itemised_sales_register/eie_gst_itemised_sales_register.py
@@ -6,12 +6,12 @@ from eie.eie.report.eie_item_wise_sales_register.eie_item_wise_sales_register im
 
 def execute(filters=None):
 	return _execute(filters, additional_table_columns=[
-		dict(fieldtype='Data', label='Customer GSTIN', fieldname="customer_gstin", width=120),
+		# dict(fieldtype='Data', label='Customer GSTIN', fieldname="customer_gstin", width=120),
 		dict(fieldtype='Data', label='Company GSTIN', fieldname="company_gstin", width=120),
 		dict(fieldtype='Data', label='HSN Code', fieldname="hsn_code", width=120),
 		dict(fieldtype='Data', label='Stock UOM', fieldname="stock_uom", width=60)
 	], additional_query_columns=[
-		'customer_gstin',
+		# 'customer_gstin',
 		'company_gstin',
 		'gst_hsn_code',
 		'stock_uom'
